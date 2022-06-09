bb84683657fd0093b4bbee67326a8708fb4034a5
Ayushi Jain <97507160+ayushi8855@users.noreply.github.com>
2022-06-10 01:27:42 +0530

    Add files via upload

--- crud/src/controller/usercont.js
+++ crud/src/controller/usercont.js
@@ -36,4 +36,30 @@ router.patch("/:id",async(req,res)=>{
         return res.status(500).send(err.message)
     }
 })
+router.get("/:id/addresses",async(req,res)=>{
+    try{
+        const user = await User.find({"_id":req.params.id},{"address":1,"_id":0}).lean().exec()
+        return res.send(user)
+    }catch(error){
+        return res.status(500).send(error.message)
+    }
+   
+})
+
+router.post("/:id/addresses",async(req,res)=>{
+    try{const user = await User.findByIdAndUpdate(req.params.id,{ $push: {"address": req.body}}).lean().exec()
+    return res.send(user)}
+    catch(error){
+        return res.status(500).send(error.message)
+    }
+})
+
+router.patch("/:id/addresses/:index",async(req,res)=>{
+    try{ var index = req.params.index
+        const user = await User.findOneAndUpdate({"_id": req.params._id},{$set:{[`address.${index}`]: req.body}})
+        return res.send(user)}
+        catch(error){
+            return res.status(500).send(error.message)
+        }
+})
 module.exports=router
--- crud/src/models/usermodel.js
+++ crud/src/models/usermodel.js
@@ -7,12 +7,12 @@ const userSchema= new mongoose.Schema({
   last_name:{type:String,required:true},
   mobile:{type:Number},
   email:{type:String},
-  address:{
+  address:[{
     house_no:{type:String},
     street:{type:String},
     city:{type:String},
     state:{type:String}
-  },
+  }],
  
 })
 const user = mongoose.model("user",userSchema)
